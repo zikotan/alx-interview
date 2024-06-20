bb39d103bc5e175ed3edb32d3cd26e62506de3da
zikotan <tananzakaria630@gmail.com>
2024-06-20 02:52:02 +0100

    response

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,38 @@
 #!/usr/bin/python3
-
-'''A script for parsing HTTP request logs.
-'''
+"""reads stdin line by line and computes metrics"""
 
 import sys
 
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+total_size = 0
+counter = 0
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+dict_counter = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            code = line_list[-2]
+            size = line_list[-1]
+            if code in codes:
+                dict_counter[code] += 1
+            total_size += int(size)
+            counter += 1
 
-        stlist = line.split()
-        count += 1
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+            counter = 0
 
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))

f948691c150521e9336ad90b64eea8c0f5658934
zikotan <136013382+zikotan@users.noreply.github.com>
2024-08-06 17:28:09 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,17 @@
 #!/usr/bin/node
 
-const mainUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
 
-// Main Request
-request(mainUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const urlArray = JSON.parse(body).characters;
-    myAsyn(urlArray);
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-
-// Sub request function
-function subRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error2, response2, body2) {
-      if (error2) {
-        reject(error2);
-      } else {
-        resolve(JSON.parse(body2).name);
-      }
-    });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
-
-// Asynchronous function awaits for
-// sub request resolution after each iterations
-async function myAsyn (urlArray) {
-  for (const url of urlArray) {
-    const character = await subRequest(url);
-    console.log(character);
-  }
-}
+};
